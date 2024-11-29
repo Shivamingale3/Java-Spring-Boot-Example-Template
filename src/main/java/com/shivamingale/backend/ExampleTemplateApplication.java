ce7edba2e164faa8a2b4e0313e2f1d94eddbda28
Shivamingale3 <shivamingale3@gmail.com>
2024-11-29 12:22:28 +0530

    Rename to ExampleTemplateApplication

--- src/main/java/com/shivamingale/backend/ExampleTemplateApplication.java
+++ src/main/java/com/shivamingale/backend/ExampleTemplateApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ShivamIngaleBackendApplication {
+public class ExampleTemplateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShivamIngaleBackendApplication.class, args);
+        SpringApplication.run(ExampleTemplateApplication.class, args);
     }
 
     @Bean
